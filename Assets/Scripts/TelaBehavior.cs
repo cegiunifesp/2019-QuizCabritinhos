c0398cc57a917be74f12f6d23ee5c81f914f4f0c
renanruan <37370013+renanruan@users.noreply.github.com>
2019-05-02 21:08:00 -0300

    Disposição das Telas

---
+++ Assets/Scripts/TelaBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelaBehavior : MonoBehaviour {
+
+    [Header("Identificação")]
+    public int index;
+
+    [Header("Animações")]
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        transform.position = new Vector2(0, 0);
+        transform.SetSiblingIndex(index: index);
+
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void PlayFadeIn()
+    {
+        anim.SetBool("Show", true);
+    }
+
+    public void PlayFadeOut()
+    {
+        anim.SetBool("Show", false);
+    }
+}
