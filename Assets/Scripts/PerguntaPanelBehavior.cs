d123dab243dd3f3d76da06e30f45c31ccdfcd6a0
renanruan <37370013+renanruan@users.noreply.github.com>
2019-05-03 15:01:24 -0300

    Logica das Perguntas

---
+++ Assets/Scripts/PerguntaPanelBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PerguntaPanelBehavior : MonoBehaviour {
+
+    private PerguntaScript pergunta;
+    public Text perguntaText;
+    public Text[] respText;
+
+    private PerguntaManagerBehavior pai;
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void SetPergunta(PerguntaScript p)
+    {
+        pergunta = p;
+
+        perguntaText.text = p.question;
+
+        respText[(int)Random.Range(0, 3)].text = p.rightAnsw;
+
+        foreach(string s in p.wrongAnsw)
+        {
+            int id = (int)Random.Range(0, 3);
+
+            while(respText[id].text != "New Text")
+            {
+                id = (id + 1) % 4;
+            }
+
+            respText[id].text = s;
+        }
+    }
+
+    public void SetPai(PerguntaManagerBehavior pai)
+    {
+        this.pai = pai;
+    }
+
+    public void Click(Text t)
+    {
+        if(t.text == pergunta.rightAnsw)
+        {
+            //anim Certo
+            print("Certo");
+        }
+        else
+        {
+            //anim Errado
+            print("Errado");
+        }
+    }
+
+    public void FadeOut()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void FadeIn()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ClickNext()
+    {
+        pai.NextButton();
+    }
+
+    public void ClickPrev()
+    {
+        pai.PreviousButton();
+    }
+}
--- Assets/Scripts/TelaBehavior.cs
+++ Assets/Scripts/TelaBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TelaBehavior : MonoBehaviour {
 
@@ -10,12 +11,22 @@ public class TelaBehavior : MonoBehaviour {
     [Header("Animações")]
     private Animator anim;
 
+    [Header("Animações")]
+    public UnityEvent StartFI;
+    public UnityEvent StartFO;
+    public UnityEvent EndFI;
+    public UnityEvent EndFO;
+
     // Use this for initialization
     void Start () {
-        transform.position = new Vector2(0, 0);
-        transform.SetSiblingIndex(index: index);
+        
+        transform.position = new Vector2(960, 540);
+        transform.SetSiblingIndex(index: 4-index);
 
         anim = GetComponent<Animator>();
+
+        if (index > 0)
+            PlayFadeOut();
     }
 	
 	// Update is called once per frame
@@ -33,5 +44,25 @@ public class TelaBehavior : MonoBehaviour {
     public void PlayFadeOut()
     {
         anim.SetBool("Show", false);
+    }
+
+    public void StartFadeIn()
+    {
+
+    }
+
+    public void StartFadeOut()
+    {
+
+    }
+
+    public void EndFadeIn()
+    {
+
+    }
+
+    public void EndFadeOut()
+    {
+
     }
 }
